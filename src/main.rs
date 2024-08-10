ee1e6f204ac4e657e09515604a7fb291b9b4b38f
axeladelwyn <85653279+axeladelwyn@users.noreply.github.com>
2024-08-10 16:33:52 +0700

    added some secret number

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,12 @@
 use std::io;
+use rand::Rng;
 
 fn main() {
     println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1..=100);   
+
+    println!("The secret number is: {secret_number}");
 
     println!("Please input your guess.");
 
@@ -10,11 +15,7 @@ fn main() {
     io::stdin()
         .read_line(&mut guess)
         .expect("Failed to read line");
+    
     println!("You guessed: {}", guess);
-
-    let x = 5;
-    let y = 20;
-
-    println!("x= {x} and y + 2 = {}", y + 2);
 
 }
