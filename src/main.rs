e207a7d8c2e2693aa2dc2fba32f1b17579a8057c
axeladelwyn <85653279+axeladelwyn@users.noreply.github.com>
2024-08-10 19:58:49 +0700

    added secret number

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,38 @@
 // using libraries external crate to use in this program
-use std::io;
 use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
 
 fn main() {
     println!("Guess the number!");
 
     // assigning variable to random number generator from 1-100
-    let secret_number = rand::thread_rng().gen_range(1..=100);   
+    let secret_number = rand::thread_rng().gen_range(1..=100);
 
     println!("The secret number is: {secret_number}");
 
-    println!("Please input your guess.");
 
-    let mut guess = String::new();
-    
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
-    
-    println!("You guessed: {}", guess);
+    loop {
+        println!("Please input your guess.");
 
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u32 = guess.trim().parse().expect("please type a number!");
+
+        println!("You guessed: {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            
+            }  
+        }  
+    }
 }
